453595598e7a67538433a872be7aee2d1bf8dacc
Jesica Vazquez <jevazquez@ws16.labo4>
2015-11-03 19:38:00 -0300

    ej2 con input/out terminados

--- ej2/ej2.cpp
+++ ej2/ej2.cpp
@@ -23,106 +23,6 @@ double get_time() {
 }
 */
 //Funciones y datos utilizados para la toma de tiempos
-
-//int evaluarTests(std::string fileTestData, std::string fileTestResult, std::string fileTestWrite) {
-//  std::string line;
-//  std::ifstream fileData (fileTestData.c_str());
-//  std::ifstream fileResult (fileTestResult.c_str());
-//  std::ofstream fileWrite (fileTestWrite.c_str());
-//  std::string s;
-//  std::string res;
-//  int z = 1;
-//  // Abri los archivos de datos y resultados
-//  // e instancie las variables necesarias para el problema
-//  // el vector de exploradoras y el vector de amistades 
-//
-//
-//  while (getline (fileData, line)) {
-//    Grafo grafo;
-//    int n;
-//    int m;
-//    int c;
-//
-//    std::istringstream iss(line);
-//
-//    iss >> n;
-//    iss >> m;
-//    iss >> c;
-//
-//    for (int i = 0 ; i < n ; ++i) {
-//      getline(fileData, line);
-//      std::istringstream iss(line);
-//
-//      int cantidad_colores;
-//      iss >> cantidad_colores;
-//
-//      std::set<int> colores; 
-//      for (int j = 0 ; j < cantidad_colores ; ++j) {
-//        int color;
-//        iss >> color;
-//        colores.insert(color);
-//      }
-//
-//      grafo.agregar_vertice(colores);
-//
-//    }
-//
-//    for (int i = 0 ; i < m ; ++i) {
-//      getline(fileData, line);
-//      std::istringstream iss(line);
-//
-//      int v1;
-//      int v2;
-//      iss >> v1;
-//      iss >> v2;
-//
-//      grafo.agregar_arista(v1, v2);
-//    }
-//
-//
-//
-//    bool res = list_coloring_backtracking(grafo);
-//
-//    grafo.imprimir();
-//
-//    //getline(fileResult, line);
-//    //if (res) {
-//    ////imprimo coloreo del grafo
-//    //  for (int i = 0; i < grafo.cant_vertices(), ++i) { 
-//    //    
-//    //  }
-//    //} else {
-//    ////devuelvo X
-//    //}
-//    //double prom = acum/100;
-//    //FileWrite << "Test numero: " << i << " cantidad de pisos: " << cant_pisos << std::endl;
-//    //FileWrite << std::fixed << acum << std::endl;
-//    //fileWrite << std::fixed << prom << std::endl;
-//    //acum = 0;
-//
-//
-//    //getline (fileResult, line);
-//
-//    //// Lei una linea del archivo de resultados
-//    //// y pregunto si ya termine de evaluar todos los tests
-//
-//    //int resTest = atoi(line.c_str());
-//    ////// convierto a int
-//
-//    //if (res == resTest) {
-//    //  std::cout << "Paso el test " << i << ". Felicitaciones!" << std::endl;
-//    //} else {
-//    //  std::cout << "Fallo el test " << i << ". :(" << std::endl;
-//    //  std::cout << "Obtuve " << res << " deberia tener " << resTest << std::endl;
-//    //}
-//
-//    //std::cout << z << std::endl;
-//    ++z;
-//
-//  }
-//  return 0;
-//}
-
 void borrar_subconjunto(std::set<int> &c1, std::set<int> &c2) {
   std::set<int>::iterator it;
   for (int i : c2) {
@@ -136,36 +36,33 @@ bool list_coloring_desde_nodo(Grafo &g, int actual, int padre, int color){
   
   bool pude_pintar = false;
 
+  std::set<int> colores = g.dame_colores_posibles(actual); //colores posibles para ese vertice
+  std::set<int> colores_vecinos = g.conjunto_colores_vecinos(actual); //colores de los vecinos (aquellos que ya estan definidos)
+  borrar_subconjunto(colores, colores_vecinos); //elimino los colores de mis vecinos, ya que no puedo utilizarlos
+
+  std::set<int> vecinos = g.dame_vecinos_no_visitados(actual); //conjunto de vecinos no visitados de actual
+
   //caso base
 
-  if (g.dame_vecinos_no_visitados(actual).size() == 0) {
-  //estoy en una "hoja"
-    std::set<int> aux = g.dame_colores_posibles(actual);
-    std::set<int> aux2 = g.conjunto_colores_vecinos(actual);
-    borrar_subconjunto(aux, aux2);
-    if (!aux.empty()) {
-      std::set<int>::iterator it = aux.begin();
-      g.pintar(actual, *it);
+  if (vecinos.size() == 0) {
+  //todos mis vecinos fueron coloreados
+    if (!colores.empty()) { //si me queda algun color disponible
+      std::set<int>::iterator it = colores.begin();
+      g.pintar(actual, *it); //elijo uno arbitrariamente y pinto
       pude_pintar = true;
-    } else {
-      g.pintar(actual, -1);
+    } else { //si no quedan colores disponibles, retorno false
+      g.pintar(actual, -1); 
       pude_pintar = false;
     }
   } else {
 
     //paso recursivo
 
-    std::set<int> colores = g.dame_colores_posibles(actual);
-    std::set<int> colores_vecinos = g.conjunto_colores_vecinos(actual);
-    borrar_subconjunto(colores, colores_vecinos);
-
     for (std::set<int>::iterator it_c = colores.begin(); it_c != colores.end() && !pude_pintar; ++it_c) {
     //mientras tenga colores no utilizados, y no hay podido pintar el grafo  
       //if (*it_c != color){ //pinto sólo si no es igual al color de mi padre
         g.pintar(actual, *it_c); //pinto actual de un color determinado
         pude_pintar = true;
-
-        std::set<int> vecinos = g.dame_vecinos_no_visitados(actual);
         
         for (std::set<int>::iterator it = vecinos.begin(); it!=vecinos.end() && pude_pintar; ++it) {
           if (!list_coloring_desde_nodo(g, *it, actual, *it_c)) {
@@ -184,6 +81,82 @@ bool list_coloring_desde_nodo(Grafo &g, int actual, int padre, int color){
 bool list_coloring_backtracking(Grafo &g){
   return list_coloring_desde_nodo(g, 0, 0, -1);
 }
+
+int evaluarTests(std::string fileTestData, std::string fileTestResult/*, std::string fileTestWrite*/) {
+  std::string line;
+  std::ifstream fileData (fileTestData.c_str());
+  std::ofstream fileResult (fileTestResult.c_str());
+//  std::ofstream fileWrite (fileTestWrite.c_str());
+  std::string s;
+  std::string res;
+  // Abri los archivos de datos y resultados
+  // e instancie las variables necesarias para el problema
+  // el vector de exploradoras y el vector de amistades 
+
+
+  while (getline (fileData, line)) {
+    Grafo grafo;
+    int n;
+    int m;
+    int c;
+
+    std::istringstream iss(line);
+
+    iss >> n;
+    iss >> m;
+    iss >> c;
+
+    for (int i = 0 ; i < n ; ++i) {
+      getline(fileData, line);
+      std::istringstream iss(line);
+
+      int cantidad_colores;
+      iss >> cantidad_colores;
+
+      std::set<int> colores; 
+      for (int j = 0 ; j < cantidad_colores ; ++j) {
+        int color;
+        iss >> color;
+        colores.insert(color);
+      }
+
+      grafo.agregar_vertice(colores);
+
+    }
+
+    for (int i = 0 ; i < m ; ++i) {
+      getline(fileData, line);
+      std::istringstream iss(line);
+
+      int v1;
+      int v2;
+      iss >> v1;
+      iss >> v2;
+
+      grafo.agregar_arista(v1, v2);
+    }
+
+    bool hay_solucion = list_coloring_backtracking(grafo);
+
+    //grafo.imprimir();
+
+    //getline(fileResult, line);
+    //std::istringstream is(line);
+    if (hay_solucion) { //imprimo coloreo del grafo
+
+      for (int i = 0; i < grafo.cant_vertices(); ++i) { 
+        fileResult << grafo.dame_color(i); 
+        fileResult << " "; 
+      }
+
+    } else { //devuelvo X
+      fileResult << "X"; 
+    }
+    fileResult << std::endl;
+  }
+  return 0;
+}
+
 
 void test_c3_coloreable() {
   
@@ -466,18 +439,25 @@ void test_k33_coloreable() {
   g.imprimir();
 }
 
-int main(/*int argc, char** argv*/) {
-  //std::string fileTestData(argv[1]);
-  //std::string fileTestResult(argv[2]);
+int main(int argc, char** argv) {
+  std::string fileTestData(argv[1]);
+  std::string fileTestResult(argv[2]);
   //std::string fileTestWrite(argv[3]);
   // Recibo por parametro tres archivos
   // El primero del cual leo los datos a evaluar
   // El segundo en el cual evaluo si los resultados fueron correctos
   // El tercero donde puedo escribir datos (tiempos)
 
-  test_k33_coloreable();
+  //test_k33_coloreable();
+  //test_barriletes_unidos_no_coloreable(); 
+  //test_barriletes_unidos_coloreable();
+  //test_k6_no_coloreable(); 
+  //test_k6_coloreable(); 
+  //test_k4_coloreable(); 
+  //test_c3_no_coloreable(); 
+  //test_c3_coloreable();
 
-  //evaluarTests(fileTestData, fileTestResult, fileTestWrite);
+  evaluarTests(fileTestData, fileTestResult/*, fileTestWrite*/);
   
   return 0;
 }
