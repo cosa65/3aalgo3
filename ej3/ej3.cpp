7f008db9d497fce6fbc40dbd550b168772583812
joaquin <joacoarribas@hotmail.com>
2015-11-03 23:44:55 -0300

    terminado ejercicio 3, sin testear

--- ej3/ej3.cpp
+++ ej3/ej3.cpp
@@ -28,168 +28,126 @@ double get_time() {
 //Funciones y datos utilizados para la toma de tiempos
 //
 
-void goloso_por_colores_posibles_vertice(Grafo& g) {
+int dame_el_de_maxima_aparicion(std::map<int, int>& colores_usados) {
+  int max = 0;
+  int color;
+  for (std::pair<const int, int>& par : colores_usados) {
+    if (par.second > max) {
+      max = par.second;
+      color = par.first;
+    }
+  }
+  for (std::pair<const int, int>& par : colores_usados) {
+    if (par.first == color)
+      par.second = 0;
+  }
+  if (max == 0) 
+    color = -1;
+  return color;
+}
+
+void pintar_vertices(Grafo& g, std::priority_queue<Vertice> vertices) { //std::priority_queue<Vertice> vertices) {
   int vistos = 0;
   int cant_vertices = g.cant_vertices();
-
-  std::priority_queue<Vertice> vertices_usados;
-
   // Creo vector para obtener el maximo
   for (int i = 0 ; i < cant_vertices ; ++i) {
-    vertices_usados.push(g.dame_vertice(i));
+    vertices.push(g.dame_vertice(i));
   }
 
   // Recorro todos los vertices del grafo
   while (vistos < cant_vertices) {
 
     // Hallar el vertice de mayor grado
-    Vertice vertice = vertices_usados.top();
-    vertices_usados.pop();
+    Vertice vertice = vertices.top();
+    std::cout << "vertice: " << vertice.dame_nombre() << std::endl;
+    vertices.pop();
     std::set<int> colores_vertice = vertice.dame_colores_posibles(); 
 
     // Creo un diccionario para ver cual es el color 
-    // que mas se repite entre los vecinos
+    // que mas usan los vecinos 
+    // y otro para ver cual es el que mas se repite entre los 
+    // colores posibles de los vecinos
     std::map<int, int> colores_usados;
+    std::map<int, int> colores_posibles;
     for (int color : colores_vertice) {
       colores_usados.insert(std::pair<int, int> (color, 0));
+      colores_posibles.insert(std::pair<int, int> (color, 0));
     }
 
     // recorro los vecinos del maximo
     std::set<int> vecinos = g.dame_vecinos(vertice.dame_nombre());
     for (int v : vecinos) {
 
-      // Si tengo mas de un color disponible para pintarlo al maximo
-      if (colores_vertice.size() > 1) {
+      int color_vecino = g.dame_color(v);
+      if (color_vecino != -1) { // Ya esta pintado el vecino y no puedo usar ese color
+        // Me fijo si el vertice sobre el cual estoy parado tiene a ese 
+        // color como disponible y de ser así lo aumento en el diccionario de colores
+        std::set<int>::iterator it = colores_vertice.find(color_vecino);
+        if (it != colores_vertice.end())
+          colores_usados[color_vecino]++;
 
-        int color_vecino = g.dame_color(v);
-        if (color_vecino != -1) { // Ya esta pintado el vecino y no puedo usar ese color
-          // Me fijo si el de grado maximo tiene ese color como disponible
-          std::set<int>::iterator it = colores_vertice.find(color_vecino);
-          if (it != colores_vertice.end()) { 
-            // Si es asi lo borro del conjunto de colores y del diccionario
-            colores_vertice.erase(it);
-            std::map<int, int>::iterator it = colores_usados.find(color_vecino);
-            colores_usados.erase(it);
-          }
+      } else {
+        // Si no esta pintado el vecino
+        std::set<int> colores_vecino = g.dame_colores_posibles(v);
 
-        } else {
-          // Si no esta pintado el vecino
-          std::set<int> colores_vecino = g.dame_colores_posibles(v);
-
-          // Por cada color que entre en conflicto sumo 1 en el diccionario
-          for (int color : colores_vecino) {
-            std::set<int>::iterator it = colores_vertice.find(color);
-            if (it != colores_vertice.end()) {
-              colores_usados[color]++;
-            }
+        // Por cada color que entre en conflicto sumo 1 en el diccionario
+        for (int color : colores_vecino) {
+          std::set<int>::iterator it = colores_vertice.find(color);
+          if (it != colores_vertice.end()) {
+            colores_posibles[color]++;
           }
         }
       }
     }
+    // Tengo dos diccionarios que determinan para cada color que tiene el vértice
+    // la cantidad de vecinos que tienen ese color pintado
+    // y la cantidad de vecinos que tienen ese color como posibilidad
+    
+    // Recorro el diccionario de colores usados por mis vecinos 
+    for (int i = 0 ; i < colores_usados.size() ; ++i) {
+      if (colores_vertice.size() > 1) {
+        // Elijo el color que es el mas utilizado por mis vecinos
+        int color_con_mas_apariciones = dame_el_de_maxima_aparicion(colores_usados);
+        // Lo borro
+        if (color_con_mas_apariciones != -1 )
+          colores_vertice.erase(color_con_mas_apariciones);
+      }
+    }
+
+    // O salgo del for anterior con muchos colores disponibles porque mis vecinos no
+    // estaban pintados
+    // o tengo un único color disponible para pintarme
     int color;
-    int min = 99;
-    for (std::pair<const int, int>& par : colores_usados) {
-      if (par.second < min) {
-        min = par.second;
-        color = par.first;
+    if (colores_vertice.size() == 1) {
+      // Si me queda un único color en mi conjunto es el que menos conflictos me causaba
+      // por ende pinto al grafo de esa manera
+      std::set<int>::iterator it = colores_vertice.begin();
+      color = *it;
+    } else {
+      // Si me queda más de un color disponible busco en los posibles colores 
+      // de mis vecinos el que quizas me trae menos conflictos
+      int min = 999;
+      for (std::pair<const int, int>& par : colores_posibles) {
+        if (par.second < min) {
+          min = par.second;
+          color = par.first;
+        }
       }
     }
     g.pintar(vertice.dame_nombre(), color);
     vistos++;
   }
+}
 
+void goloso_por_colores_posibles_vertice(Grafo& g) {
+  std::priority_queue<Vertice> vertices;
+  pintar_vertices(g, vertices);
 }
 
 void goloso_por_grado_vertice(Grafo& g) {
-  int vistos = 0;
-  int cant_vertices = g.cant_vertices();
-
-  std::priority_queue<Vertice, std::vector<Vertice>, std::greater<Vertice> > vertices_usados;
-
-  // Creo vector para obtener el maximo
-  for (int i = 0 ; i < cant_vertices ; ++i) {
-    vertices_usados.push(g.dame_vertice(i));
-  }
-
-  // Recorro todos los vertices del grafo
-  while (vistos < cant_vertices) {
-
-    // Hallar el vertice de mayor grado
-    Vertice vertice = vertices_usados.top();
-    vertices_usados.pop();
-    std::set<int> colores_vertice = vertice.dame_colores_posibles(); 
-
-    // Creo un diccionario para ver cual es el color 
-    // que mas se repite entre los vecinos
-    std::map<int, int> colores_usados;
-    for (int color : colores_vertice) {
-      colores_usados.insert(std::pair<int, int> (color, 0));
-    }
-
-    // recorro los vecinos del maximo
-    std::set<int> vecinos = g.dame_vecinos(vertice.dame_nombre());
-    for (int v : vecinos) {
-
-      // Si tengo mas de un color disponible para pintarlo al maximo
-      if (colores_vertice.size() > 1) {
-
-        int color_vecino = g.dame_color(v);
-        if (color_vecino != -1) { // Ya esta pintado el vecino y no puedo usar ese color
-          // Me fijo si el de grado maximo tiene ese color como disponible
-          std::set<int>::iterator it = colores_vertice.find(color_vecino);
-          if (it != colores_vertice.end()) { 
-            // Si es asi lo borro del conjunto de colores y del diccionario
-            colores_vertice.erase(it);
-            std::map<int, int>::iterator it = colores_usados.find(color_vecino);
-            colores_usados.erase(it);
-          }
-
-        } else {
-          // Si no esta pintado el vecino
-          std::set<int> colores_vecino = g.dame_colores_posibles(v);
-
-          // Por cada color que entre en conflicto sumo 1 en el diccionario
-          for (int color : colores_vecino) {
-            std::set<int>::iterator it = colores_vertice.find(color);
-            if (it != colores_vertice.end()) {
-              colores_usados[color]++;
-            }
-          }
-        }
-      }
-    }
-    int color;
-    int min = 99;
-    for (std::pair<const int, int>& par : colores_usados) {
-      if (par.second < min) {
-        min = par.second;
-        color = par.first;
-      }
-    }
-    g.pintar(vertice.dame_nombre(), color);
-    vistos++;
-  }
+//  std::priority_queue<Vertice, std::vector<Vertice>, std::greater<Vertice> > vertices;
+//  pintar_vertices(g, vertices);
 }
-
-// Una vez que tengo el conjunto de colores del vertice tengo que verificar
-// 1) Filtrar los colores que ya estan siendo utilizados por los vertices vecinos 
-// 2) Luego, por el color del vertice que menos se interseco con los de sus vecinos 
-// 3) 
-// si ya esta sinedo utilizado
-//  si el set tiene tamaño 1
-//    no hacer nada
-//  sino 
-//    sacar ese color
-//
-//  colol = Vector de vectores [[color, numero de veces que aparecio]]
-//  sino esta siendo utilizado
-//    para cada conjunto de colores del vecino
-//      si esta en colol
-//        aumentar su numero
-//
-//  sacar el minimo color tal que su numero de veces sea el menor
-//      
-//  pintar al vertice de ese color
 
 int evaluarTests(std::string fileTestData, std::string fileTestResult, std::string fileTestWrite) {
   std::string line;
