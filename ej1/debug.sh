c7753bd31429dbf5a5e48e1cc6d2f79ce6cf0abf
Agustin <azavalla@dc.uba.ar>
2015-11-14 20:30:13 -0300

    run

---
+++ ej1/debug.sh
@@ -0,0 +1,1 @@
+cgdb --command=breakpoints --args ./ej1 prueba/files/testEj1.in prueba/files/testEj1.out prueba/files/tiempos.out
---
+++ ej1/run.sh
@@ -0,0 +1,1 @@
+echo -e '\0033\0143' && make clean && make && ./ej1 prueba/files/testEj1.in prueba/files/testEj1.out prueba/files/tiempos.out
