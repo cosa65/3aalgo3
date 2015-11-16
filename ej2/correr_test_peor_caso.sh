382773bb425b2aa20b307c64c7ba1135cfada235
Jesica Vazquez <jevazquez@ws1.labo2>
2015-11-16 15:42:38 -0300

    tiempos mejor caso

---
+++ ej2/correr_test_peor_caso.sh
@@ -0,0 +1,9 @@
+./ej2 tests_tiempos/mejor_caso/mejor_caso_3_vertices.in hare tests_tiempos/resultados_mejor_caso/res_3_vertices &&
+./ej2 tests_tiempos/mejor_caso/mejor_caso_4_vertices.in hare tests_tiempos/resultados_mejor_caso/res_4_vertices &&
+./ej2 tests_tiempos/mejor_caso/mejor_caso_5_vertices.in hare tests_tiempos/resultados_mejor_caso/res_5_vertices &&
+./ej2 tests_tiempos/mejor_caso/mejor_caso_6_vertices.in hare tests_tiempos/resultados_mejor_caso/res_6_vertices &&
+./ej2 tests_tiempos/mejor_caso/mejor_caso_7_vertices.in hare tests_tiempos/resultados_mejor_caso/res_7_vertices &&
+./ej2 tests_tiempos/mejor_caso/mejor_caso_8_vertices.in hare tests_tiempos/resultados_mejor_caso/res_8_vertices &&
+./ej2 tests_tiempos/mejor_caso/mejor_caso_9_vertices.in hare tests_tiempos/resultados_mejor_caso/res_9_vertices &&
+./ej2 tests_tiempos/mejor_caso/mejor_caso_10_vertices.in hare tests_tiempos/resultados_mejor_caso/res_10_vertices &&
+./ej2 tests_tiempos/mejor_caso/mejor_caso_11_vertices.in hare tests_tiempos/resultados_mejor_caso/res_11_vertices 
