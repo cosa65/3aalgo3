6c0ed72371453386483409b407ac30241425fcea
cosa65 <mfosc65@gmail.com>
2015-11-18 13:26:23 -0300

    tiempos ultima esperanza

--- ej4/tiempos/tiempos.sh
+++ ej4/tiempos/tiempos.sh
@@ -4,33 +4,47 @@ make build
 j=0
 for ((i = 1; i <= 15; i++));
 do
-	./random 1000 0 30 resultados/times.in
+	./random 500 0 100 resultados/times.in
 	./ej4Time resultados/times.in test.txt resultados/times.out vecinos i
 	./ej4Time resultados/times.in test.txt resultados/times.out individual i
 done
 for ((i = 1; i <= 15; i++));
 do
-	./random 900 0 30 resultados/times.in
+	./random 500 0 90 resultados/times.in
 	./ej4Time resultados/times.in test.txt resultados/times.out vecinos i
 	./ej4Time resultados/times.in test.txt resultados/times.out individual i
 done
 for ((i = 1; i <= 15; i++));
 do
-	./random 800 0 30 resultados/times.in
-	./ej4Time resultados/times.in test.txt resultados/times.out vecinos i
-	./ej4Time resultados/times.in test.txt resultados/times.out individual i
-done
-
-for ((i = 1; i <= 15; i++));
-do
-	./random 700 0 30 resultados/times.in
+	./random 500 0 80 resultados/times.in
 	./ej4Time resultados/times.in test.txt resultados/times.out vecinos i
 	./ej4Time resultados/times.in test.txt resultados/times.out individual i
 done
 
 for ((i = 1; i <= 15; i++));
 do
-	./random 600 0 30 resultados/times.in
+	./random 500 0 70 resultados/times.in
+	./ej4Time resultados/times.in test.txt resultados/times.out vecinos i
+	./ej4Time resultados/times.in test.txt resultados/times.out individual i
+done
+
+for ((i = 1; i <= 15; i++));
+do
+	./random 500 0 60 resultados/times.in
+	./ej4Time resultados/times.in test.txt resultados/times.out vecinos i
+	./ej4Time resultados/times.in test.txt resultados/times.out individual i
+done
+
+for ((i = 1; i <= 15; i++));
+do
+	./random 500 0 50 resultados/times.in
+	./ej4Time resultados/times.in test.txt resultados/times.out vecinos i
+	./ej4Time resultados/times.in test.txt resultados/times.out individual i
+done
+
+for ((i = 1; i <= 15; i++));
+do
+	./random 500 0 40 resultados/times.in
 	./ej4Time resultados/times.in test.txt resultados/times.out vecinos i
 	./ej4Time resultados/times.in test.txt resultados/times.out individual i
 done
@@ -44,28 +58,14 @@ done
 
 for ((i = 1; i <= 15; i++));
 do
-	./random 400 0 30 resultados/times.in
+	./random 500 0 20 resultados/times.in
 	./ej4Time resultados/times.in test.txt resultados/times.out vecinos i
 	./ej4Time resultados/times.in test.txt resultados/times.out individual i
 done
 
 for ((i = 1; i <= 15; i++));
 do
-	./random 300 0 30 resultados/times.in
-	./ej4Time resultados/times.in test.txt resultados/times.out vecinos i
-	./ej4Time resultados/times.in test.txt resultados/times.out individual i
-done
-
-for ((i = 1; i <= 15; i++));
-do
-	./random 200 0 30 resultados/times.in
-	./ej4Time resultados/times.in test.txt resultados/times.out vecinos i
-	./ej4Time resultados/times.in test.txt resultados/times.out individual i
-done
-
-for ((i = 1; i <= 15; i++));
-do
-	./random 100 0 30 resultados/times.in
+	./random 500 0 10 resultados/times.in
 	./ej4Time resultados/times.in test.txt resultados/times.out vecinos i
 	./ej4Time resultados/times.in test.txt resultados/times.out individual i
 done
