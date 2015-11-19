e259b7eda5325eefe0d743a1862a7dfe76c5eb80
Guido <joacoarribas@hotmail.com>
2015-11-19 17:48:16 +0000

    tiempos del 2 casi terminado

--- ej2/creacion_tests/crear_test_2lc.py
+++ ej2/creacion_tests/crear_test_2lc.py
@@ -1,16 +1,16 @@
 import random
 from sets import Set
 
-f = open('tests_peor_caso_colores_arbol.in', 'w')
-vertex = 3 
+f = open('tests_sin_2lc.in', 'w')
+vertex = 4
 
 #i = 0
-while (vertex <= 11):
-  edges = ((vertex-1)*(vertex-2)/2) 
-  colors = 5
-  line = str(vertex) + ' ' + str(edges) + ' ' + str(colors) + '\n'
+while (vertex <= 24):
+  edges_amount = ((vertex-1)*(vertex-2)/2) 
+  colors = 10
+  line = str(vertex) + ' ' + str(edges_amount) + ' ' + str(colors) + '\n'
   f.write(line)
-  for i in range(0, a):
+  for i in range(0, vertex):
     color = random.randint(1, colors)
     col = Set()
     
@@ -30,14 +30,24 @@ while (vertex <= 11):
     f.write(line)
 
   edges = set()
-  while ()
-    v1 = random.randint(0, edges)
-    v2 = random.randint(0, edges)
-    edges.add(v1,v2)
+  while (len(edges) != edges_amount):
+    v1 = random.randint(0, vertex-1)
+    v2 = random.randint(0, vertex-1)
+    while (v1 == v2):
+      v2 = random.randint(0, vertex-1)
+
+    edge = frozenset([v1,v2])
+    edges.add(edge)
 
   for edge in edges:
-    connections =  str(edge[0]) + ' ' + str(edge[1]) + '\n' 
+    a = set()
+    for e in edge:
+      a.add(e)
+          
+    v1 = a.pop()
+    v2 = a.pop()
+    connections =  str(v1) + ' ' + str(v2) + '\n' 
     f.write(connections)
 
   print "iteracion %d" % vertex
-  vertex += 1
+  vertex += 2
