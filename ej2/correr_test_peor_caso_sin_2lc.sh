1a8c7f9233c0721c466d12d0e0f7c89f54374285
joaquin <joacoarribas@hotmail.com>
2015-11-19 10:11:54 -0300

    hare

---
+++ ej2/correr_test_peor_caso_sin_2lc.sh
@@ -0,0 +1,12 @@
+./ej2 tests_tiempos/peor_caso/peor_caso_3_vertices.in hare tests_tiempos/resultados_peor_caso_sin_2lc/res_3_vertices.txt &&
+./ej2 tests_tiempos/peor_caso/peor_caso_4_vertices.in hare tests_tiempos/resultados_peor_caso_sin_2lc/res_4_vertices.txt &&
+./ej2 tests_tiempos/peor_caso/peor_caso_5_vertices.in hare tests_tiempos/resultados_peor_caso_sin_2lc/res_5_vertices.txt &&
+./ej2 tests_tiempos/peor_caso/peor_caso_6_vertices.in hare tests_tiempos/resultados_peor_caso_sin_2lc/res_6_vertices.txt &&
+./ej2 tests_tiempos/peor_caso/peor_caso_7_vertices.in hare tests_tiempos/resultados_peor_caso_sin_2lc/res_7_vertices.txt &&
+./ej2 tests_tiempos/peor_caso/peor_caso_8_vertices.in hare tests_tiempos/resultados_peor_caso_sin_2lc/res_8_vertices.txt &&
+./ej2 tests_tiempos/peor_caso/peor_caso_9_vertices.in hare tests_tiempos/resultados_peor_caso_sin_2lc/res_9_vertices.txt &&
+./ej2 tests_tiempos/peor_caso/peor_caso_10_vertices.in hare tests_tiempos/resultados_peor_caso_sin_2lc/res_10_vertices.txt && 
+./ej2 tests_tiempos/peor_caso/peor_caso_11_vertices.in hare tests_tiempos/resultados_peor_caso_sin_2lc/res_11_vertices.txt &&
+./ej2 tests_tiempos/peor_caso/peor_caso_12_vertices.in hare tests_tiempos/resultados_peor_caso_sin_2lc/res_12_vertices.txt &&  
+./ej2 tests_tiempos/peor_caso/peor_caso_13_vertices.in hare tests_tiempos/resultados_peor_caso_sin_2lc/res_13_vertices.txt 
+exit
